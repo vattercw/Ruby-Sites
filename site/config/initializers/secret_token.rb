d258fb828797481829a9263091f0b36a4cf5039c
vattercw <vattercw@rose-hulman.edu>
2014-03-03 21:26:24 -0500

    added secondary tutorial project

---
+++ site/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Site::Application.config.secret_key_base = 'b5b5292e817e989c21433dc3143a3f92cb267c94f7d8bf9a03746d28ed57e22501228b2647febf7ac0edea1ed69eefa8d959034a35a2006e6fe477af80ec4b1a'
